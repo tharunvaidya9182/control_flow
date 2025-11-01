17ee6a0a53f86be5080a1fc7c2ae3311b68187aa
tharunvaidya <tharunvaidya77@gamil.com>
2025-11-01 15:27:22 +0530

    equal or not

---
+++ pro4.c
@@ -0,0 +1,14 @@
+//PROGRAM TO ACCEPT TWO INTEGERS AND CHECK WHETHER THEY ARE EQUAL OR NOT.
+#include<stdio.h>
+void main(){
+int number1,number2;
+printf("enter the number1: ");
+scanf("%d",&number1);
+printf("enter the number2: ");
+scanf("%d",&number2);
+if(number1==number2){
+  printf("%d is equal to %d\n",number1,number2);
+}else{
+  printf("%d is not equal to %d\n",number1,number2);
+  }
+  }
