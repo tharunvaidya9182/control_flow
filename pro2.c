27c3f1a4e8dc8f86188f45b8135c9b9a7d167e02
tharunvaidya <tharunvaidya77@gamil.com>
2025-11-01 14:43:26 +0530

    second program

---
+++ pro2.c
@@ -0,0 +1,12 @@
+//program to check weather given number is positive or negative
+#include<stdio.h>
+void main(){
+int num;
+printf("enter the number: ");
+scanf("%d",&num);
+if(num>0){
+printf("it is positive");
+}else{
+printf("it is negative");
+}
+}
